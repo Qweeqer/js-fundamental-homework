1f04d25f08352dc7b5ea5646566397ff753664e9
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-02-19 00:32:31 +0200

    add task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,24 @@
-const validation = document.querySelector("#validation-input");
-const addRemoveClass = (remove, add) => {
-  validation.classList.remove(`${remove}`);
-  validation.classList.add(`${add}`);
-};
-const validSymbol = (event) => {
-  const { value } = event.currentTarget;
-  const { length } = validation.dataset;
-    console.log(value.length);
-    console.log(Number(length) === value.length);
-  if (Number(length) === value.length) {
-    addRemoveClass("invalid", "valid");
-  } else {
-      addRemoveClass("valid", "invalid");
+function groupByType(arr) {
+  let numbersArr = [];
+  let stringsArr = [];
+
+  for (let element of arr) {
+    if (typeof element === 'number') {
+      numbersArr.push(element);
+    } else if (typeof element === 'string') {
+      stringsArr.push(element);
+    } else if (Array.isArray(element)) {
+      let subArrays = groupByType(element);
+      numbersArr = numbersArr.concat(subArrays[0]);
+      stringsArr = stringsArr.concat(subArrays[1]);
     }
-};
-validation.addEventListener("blur", validSymbol);
-console.log(validation.textContent);
+  }
+
+  return [numbersArr, stringsArr];
+}
+
+let arr = [5, 'Limit', 12, 'a', '3', 99, 2, [2, 4, 3, '33', 'a', 'text'], 'strong', 'broun'];
+let arrNew = groupByType(arr);
+console.log(arrNew);
+// [ [ 5, 12, 99, 2, 2, 4, 3 ], [ 'Limit', 'a', '3', '33', 'a', 'text', 'strong', 'broun' ] ]
+
