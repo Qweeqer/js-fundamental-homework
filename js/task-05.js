da89c0fd65a3e3be3db7e8c4b62c5b2cae20013f
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-02-18 23:17:34 +0200

    Add more data

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,20 @@
-const input = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-input.addEventListener("input", (event) => {
-  console.log(event.currentTarget.value);
-   if (event.currentTarget.value.trim() === '') {
-       return nameOutput.innerHTML = 'Anonymous';
-    } 
-  nameOutput.textContent = event.currentTarget.value;
-});
+function countNumbers(arr) {
+  let twoDigit = 0;
+  let threeDigit = 0;
+  let fourDigit = 0;
+
+  for (let num of arr) {
+    if (num >= 10 && num < 100) {
+      twoDigit += 1;
+    } else if (num >= 100 && num < 1000) {
+      threeDigit += 1;
+    } else if (num >= 1000 && num < 10000) {
+      fourDigit += 1;
+    }
+  }
+
+  return [twoDigit, threeDigit, fourDigit];
+}
+let arr = [1,2,55,3,100,333,9999];
+let counts = countNumbers(arr);
+console.log(counts);  // [1, 2, 1]
