81284b2ab3c121f94483afb38feb50a8a6b93fd1
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-02-20 23:25:46 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,31 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function getFullName(firstName, lastName, transformFunc = null) {
+  let fullName = `${firstName} ${lastName}`.trim(); // видаляє пробіли з початку і кінця рядка
+
+  fullName = fullName.replace(/\s+/g, ' '); // замінює послідовності пробілів на один пробіл
+
+  if (transformFunc) {
+    switch (transformFunc) {
+      case 'uppercase':
+        fullName = fullName.toUpperCase();
+        break;
+      case 'lowercase':
+        fullName = fullName.toLowerCase();
+        break;
+      case 'capitalize':
+        fullName = fullName.replace(/\b\w/g, firstLetter => firstLetter.toUpperCase());
+        break;
+      default:
+        console.log('Invalid transform function');
+    }
+  }
+
+  return fullName;
 }
 
-const body = document.body;
-const btnStart = document.querySelector('.change-color');
-const inputSpanColor = document.querySelector(`.color`)
 
-btnStart.addEventListener('click', changeColor);
-function changeColor() {
-  const randomColor = getRandomHexColor()
-  body.style.backgroundColor = randomColor;
-  inputSpanColor.textContent = randomColor;
-  console.log(randomColor);
-};
-
+let fullName = getFullName('John', 'Gippercase', 'uppercase');
+console.log(fullName);
+console.log(getFullName('John', 'Smith', 'uppercase')); // "JOHN Smith"
+console.log(getFullName('John', 'Smith', 'lowercase')); // "john smith"
+console.log(getFullName('john', 'smith', 'capitalize')); // "John Smith"
+console.log(getFullName('   John  ', '   Smith  ', 'capitalize')); // "John Smith"
