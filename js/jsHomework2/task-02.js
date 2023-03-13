c6fc9a44c7ac59e1d61c5211729a42787c9cdfdb
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-03-13 13:57:20 +0200

    Update task-02.js

--- js/jsHomework2/task-02.js
+++ js/jsHomework2/task-02.js
@@ -10,13 +10,42 @@
 // propsCount(mentor); // 3
 // console.log('Кількість властивостей цього об’єкта: ', propsCount(mentor)); // 3
 // Ось та ж сама функція propsCount з використанням стрілочної функції:
-
-let mentor1 = {
+// ..Варіант 2..........
+let mentor2 = {
   course: "JS fundamental",
   duration: 3,
   direction: "web-development"
 };
 
 const propsCount = currentObject => Object.keys(currentObject).length;
-propsCount(mentor1); // 3
-console.log('Кількість властивостей цього об’єкта: ', propsCount(mentor1)); // 3
+propsCount(mentor2); // 3
+console.log('Кількість властивостей цього об’єкта: ', propsCount(mentor2)); // 3
+// ..Варіант 3..
+let mentor3 = {
+  course: "JS fundamental",
+  duration: 3,
+  direction: "web-development",
+  propsCount: function () {
+    return Object.keys(this).length;
+  }
+}
+
+console.log('Кількість властивостей цього об’єкта разом з методами: ', mentor3.propsCount()); // 3
+// Варіант 4
+let mentor4 = {
+  course: "JS fundamental",
+  duration: 3,
+  direction: "web-development",
+  propsCount: function() {
+    let count = 0;
+    for (let key in this) {
+      if (typeof this[key] !== 'function') {
+        count++;
+      }
+    }
+    return count;
+  }
+};
+
+console.log('Кількість властивостей цього об’єкта без методів: ', mentor4.propsCount()); // 3
+
