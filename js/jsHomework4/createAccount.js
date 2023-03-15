2e2bcad676fffcfa6ce792d348f72f41bf24323b
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-03-15 20:44:45 +0200

    Update createAccount.js

--- js/jsHomework4/createAccount.js
+++ js/jsHomework4/createAccount.js
@@ -12,6 +12,146 @@ const inputList = document.querySelectorAll('input');
 const labelList = document.querySelectorAll('label');
 const inputs = document.querySelectorAll('.input-wrapper input');
 
+
+///Перевірка input на наявність значення для додавання класу
+inputList.forEach(function (input, index) {
+  input.addEventListener('focus', function () {
+    inputs.forEach(function (i) {
+      if (i !== input) {
+        i.style.backgroundColor = '';
+      }
+    });
+    if (input.value.trim() === '') {
+      labelList[index].classList.remove('labelListOutFocus');
+      labelList[index].classList.add('labelListOnFocus');
+      input.style.backgroundColor = 'white';
+    }
+  });
+
+  input.addEventListener('blur', function () {
+    if (input.value.trim() === '') {
+      labelList[index].classList.remove('labelListOnFocus');
+      labelList[index].classList.add('labelListOutFocus');
+      input.style.backgroundColor = '';
+    }
+  });
+  // Перевірка вводу даних в полях після першої невірної валідації для додавання класу
+  input.addEventListener('input', function () {
+    const value = input.value;
+    if (value === '') {
+      input.style.backgroundColor = '';
+      labelList[index].classList.remove('labelListOnFocus');
+      return;
+    } else {
+      labelList[index].classList.add('labelListOnFocus');
+      // input.style.backgroundColor = '';
+    }
+  });
+});
+
+function validateForm() {
+  let isValid = true;
+
+  // Перевірка усіх полів вводу та чекбокса
+  if (
+    fullNameInput.value === '' ||
+    emailInput.value === '' ||
+    phoneInput.value === '' ||
+    passwordInput.value === '' ||
+    confirmPasswordInput.value === '' ||
+    !agreeCheckbox.checked
+  ) {
+    isValid = false;
+  }
+  //Перевірка імені
+  const fullNameRegex = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
+  if (!fullNameRegex.test(fullNameInput.value)) {
+    errorMessage.textContent =
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan";
+    isValid = false;
+  }
+  // Перевірка корректності email
+  const emailRegex =
+    /^[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*\.[A-Za-z]{2,}$/;
+  if (!emailRegex.test(emailInput.value)) {
+    errorMessage.textContent = 'Please enter valid email address, for example  example @gmail.com';
+    isValid = false;
+  }
+  // Перевірка номера телефона
+  const phoneRegex =
+    /^\+\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d$/;
+  if (!phoneRegex.test(phoneInput.value)) {
+    errorMessage.textContent = 'Phone is not correct!Must be in format +XX(XXX)XXX-XX-XX';
+    isValid = false;
+  }
+  // Перевірка пароля
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[_#!])[0-9a-zA-Z_#!]{8,}$/;
+  if (!passwordRegex.test(passwordInput.value)) {
+    errorMessage.textContent =
+      'Please enter your password. Minimum length 8 symbols. Symbols _#! Min 1 Big and 1 small letter ';
+    isValid = false;
+  }
+
+  // Перевірка співпадіння паролей
+  if (passwordInput.value !== confirmPasswordInput.value) {
+    errorMessage.textContent = 'Паролі не співпадають. Будь ласка, перевірте введені дані.';
+    isValid = false;
+  }
+  submitButton.disabled = !isValid;
+  return isValid;
+}
+// **********************************************
+function updateSubmitButtonState() {
+  submitButton.disabled = !validateForm();
+}
+// Перевірка поля після першої невдалої валідації
+const inputFields = [fullNameInput, emailInput, phoneInput, passwordInput, confirmPasswordInput];
+
+inputFields.forEach(input => {
+  input.addEventListener('input', () => {
+    input.style.backgroundColor = '';
+    errorMessage.textContent = '';
+    updateSubmitButtonState();
+  });
+});
+
+agreeCheckbox.addEventListener('change', () => {
+  errorMessage.textContent = '';
+  updateSubmitButtonState();
+});
+// // Додаемо слухача подій на FullName
+// fullNameInput.addEventListener('input', () => {
+//   fullNameInput.style.backgroundColor = '';
+//   errorMessage.textContent = '';
+//   updateSubmitButtonState();
+// });
+// // Додаемо слухача подій на телефон
+// phoneInput.addEventListener('input', () => {
+//   phoneInput.style.backgroundColor = '';
+//   errorMessage.textContent = '';
+//   updateSubmitButtonState();
+// });
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  if (!validateForm()) {
+    errorMessage.textContent =
+      'Будь ласка, заповніть всі поля та погодьтесь з Умовами використання.';
+    return;
+  } else {
+    errorMessage.textContent = '';
+    alert('Ваш обліковий запис успішно створено!');
+    form.reset();
+    updateSubmitButtonState();
+  }
+  // submitButton.disabled = !isValid;
+});
+// Оновленя стану кнопки відправки при завантаженні сторінки
+updateSubmitButtonState();
+
+
+
+// Нотатки
 // form.addEventListener('submit', function (event) {
 //   event.preventDefault();
 
@@ -90,138 +230,3 @@ const inputs = document.querySelectorAll('.input-wrapper input');
 //     isValid = false;
 //   }
 // });
-///Перевірка input на наявність значення
-inputList.forEach(function (input, index) {
-  input.addEventListener('focus', function () {
-    inputs.forEach(function (i) {
-      if (i !== input) {
-        i.style.backgroundColor = '';
-      }
-    });
-    if (input.value.trim() === '') {
-      labelList[index].classList.remove('labelListOutFocus');
-      labelList[index].classList.add('labelListOnFocus');
-      input.style.backgroundColor = 'white';
-    }
-  });
-
-  input.addEventListener('blur', function () {
-    if (input.value.trim() === '') {
-      labelList[index].classList.remove('labelListOnFocus');
-      labelList[index].classList.add('labelListOutFocus');
-      input.style.backgroundColor = '';
-    }
-  });
-  // Перевірка вводу даних в полях після першої невірної валідації
-  input.addEventListener('input', function () {
-    const value = input.value;
-    if (value === '') {
-      input.style.backgroundColor = '';
-      labelList[index].classList.remove('labelListOnFocus');
-      return;
-    } else {
-      labelList[index].classList.add('labelListOnFocus');
-      // input.style.backgroundColor = '';
-    }
-  });
-});
-
-function validateForm() {
-  let isValid = true;
-
-  // Проверка всех полей ввода и чекбокса
-  if (
-    fullNameInput.value === '' ||
-    emailInput.value === '' ||
-    phoneInput.value === '' ||
-    passwordInput.value === '' ||
-    confirmPasswordInput.value === '' ||
-    !agreeCheckbox.checked
-  ) {
-    isValid = false;
-  }
-  //Перевірка імені
-  const fullNameRegex = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
-  if (!fullNameRegex.test(fullNameInput.value)) {
-    errorMessage.textContent =
-      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan";
-    isValid = false;
-  }
-  // Проверка корректности email
-  const emailRegex =
-    /^[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*\.[A-Za-z]{2,}$/;
-  if (!emailRegex.test(emailInput.value)) {
-    errorMessage.textContent = 'Please enter valid email address, for example  example @gmail.com';
-    isValid = false;
-  }
-  // Проверка корректности номера телефона
-  const phoneRegex =
-    /^\+\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d(\s|\-|\(|\)|)?\d$/;
-  if (!phoneRegex.test(phoneInput.value)) {
-    errorMessage.textContent = 'Phone is not correct!Must be in format +XX(XXX)XXX-XX-XX';
-    isValid = false;
-  }
-  // Проверка корректности пароля
-  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[_#!])[0-9a-zA-Z_#!]{8,}$/;
-  if (!passwordRegex.test(passwordInput.value)) {
-    errorMessage.textContent =
-      'Please enter your password. Minimum length 8 symbols. Symbols _#! Min 1 Big and 1 small letter ';
-    isValid = false;
-  }
-
-  // Проверка совпадения паролей
-  if (passwordInput.value !== confirmPasswordInput.value) {
-    errorMessage.textContent = 'Паролі не співпадають. Будь ласка, перевірте введені дані.';
-    isValid = false;
-  }
-  submitButton.disabled = !isValid;
-  return isValid;
-}
-// **********************************************
-function updateSubmitButtonState() {
-  submitButton.disabled = !validateForm();
-}
-// Проверка ввода данных в полях после первой неверной валидации
-const inputFields = [fullNameInput, emailInput, phoneInput, passwordInput, confirmPasswordInput];
-
-inputFields.forEach(input => {
-  input.addEventListener('input', () => {
-    input.style.backgroundColor = '';
-    errorMessage.textContent = '';
-    updateSubmitButtonState();
-  });
-});
-
-agreeCheckbox.addEventListener('change', () => {
-  errorMessage.textContent = '';
-  updateSubmitButtonState();
-});
-// Добавление слушателя событий для поля ввода FullName
-fullNameInput.addEventListener('input', () => {
-  fullNameInput.style.backgroundColor = '';
-  errorMessage.textContent = '';
-  updateSubmitButtonState();
-});
-// Добавление слушателя событий для поля ввода телефона
-phoneInput.addEventListener('input', () => {
-  phoneInput.style.backgroundColor = '';
-  errorMessage.textContent = '';
-  updateSubmitButtonState();
-});
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  if (!validateForm()) {
-    errorMessage.textContent =
-      'Будь ласка, заповніть всі поля та погодьтесь з Умовами використання.';
-    return;
-  } else {
-    errorMessage.textContent = '';
-    alert('Ваш обліковий запис успішно створено!');
-    form.reset();
-    updateSubmitButtonState();
-  }
-  // submitButton.disabled = !isValid;
-});
-// Обновление состояния кнопки отправки при загрузке страницы
-updateSubmitButtonState();
