c844b559607ddd3ec608ad8c312bd486a44c1e24
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-03-29 12:04:49 +0300

    видалення зайвих файлів

--- html/weather/note/1/weatherVariables.js
+++
@@ -1,9 +0,0 @@
-export const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
-export const apiUrlWeather = 'https://api.openweathermap.org/data/2.5/weather';
-export const apiUrlForecast =
-  'https://api.openweathermap.org/data/2.5/forecast';
-export const cityInput = document.querySelector('#city-input');
-export const weatherList = document.querySelector('#weather-list');
-export const todayButton = document.querySelector('#today-button');
-export const threeDaysButton = document.querySelector('#three-day-button');
-export const sevenDaysButton = document.querySelector('#seven-day-button');
